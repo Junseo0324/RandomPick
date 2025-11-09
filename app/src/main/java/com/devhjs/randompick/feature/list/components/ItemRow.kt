ce1ff045941422846c9edcc4aa9beceda6e2f331
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-09 18:14:16 +0900

    feat: BottomSheet 내부에서 사용할 ItemRow 파일

---
+++ app/src/main/java/com/devhjs/randompick/feature/list/components/ItemRow.kt
@@ -0,0 +1,56 @@
+package com.devhjs.randompick.feature.list.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.devhjs.randompick.core.ui.theme.Dimens
+
+
+@Composable
+fun ItemRow(
+    text: String,
+    onDelete: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+                RoundedCornerShape(Dimens.spacingSmall)
+            )
+            .padding(horizontal = Dimens.spacingLarge, vertical = Dimens.spacingMedium),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(
+            onClick = onDelete,
+            modifier = Modifier.size(Dimens.iconSizeMedium)
+        ) {
+            Icon(
+                Icons.Outlined.Delete,
+                contentDescription = "삭제",
+                tint = MaterialTheme.colorScheme.error,
+                modifier = Modifier.size(Dimens.iconSizeSmall)
+            )
+        }
+    }
+}
