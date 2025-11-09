1d7952954703762aa763df8fb942924cdc20fc11
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-09 16:32:14 +0900

    chore: Dimens 추가

---
+++ app/src/main/java/com/devhjs/randompick/core/ui/theme/Dimens.kt
@@ -0,0 +1,48 @@
+package com.devhjs.randompick.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * 앱 전반에 걸쳐 사용되는 표준 크기(Dimension) 값을 정의하는 객체입니다.
+ * 변수명은 Kotlin 컨벤션에 따라 'camelCase'를 사용합니다.
+ */
+
+object Dimens {
+
+    val spacingTiny = 4.dp
+    val spacingSmall = 8.dp
+    val spacingMedium = 12.dp
+    val spacingLarge = 16.dp
+    val spacingExtraLarge = 24.dp
+    val spacingDoubleExtraLarge = 32.dp
+
+    val screenPadding = spacingLarge
+    val cardInnerPadding = 20.dp
+    val dialogPadding = spacingExtraLarge
+
+
+    val buttonHeight = 56.dp
+    val buttonHeightSmall = 52.dp
+    val iconSizeSmall = 20.dp
+    val iconSizeMedium = 24.dp
+    val fabSize = 56.dp
+    val itemRowNumberSize = 40.dp
+    val rouletteAreaSize = 280.dp
+
+
+
+    val cornerRadiusSmall = 6.dp
+    val cornerRadiusMedium = 12.dp
+    val cornerRadiusLarge = 16.dp
+    val cornerRadiusExtraLarge = 20.dp
+
+    val cardElevation = 2.dp
+    val bottomNavElevation = 8.dp
+
+    val fontSizeSmall = 12.sp
+    val fontSizeMedium = 14.sp
+    val fontSizeLarge = 16.sp
+    val fontSizeExtraLarge = 18.sp
+    val fontSizeDialogTitle = 20.sp
+}
