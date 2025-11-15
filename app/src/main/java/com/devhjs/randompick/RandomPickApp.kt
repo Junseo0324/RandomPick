0df84bed2c3d33d15a3d3ccbb4078fa201b8a033
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-15 21:40:34 +0900

    feat: AdMob 를 활용한 광고 추가

--- app/src/main/java/com/devhjs/randompick/RandomPickApp.kt
+++ app/src/main/java/com/devhjs/randompick/RandomPickApp.kt
@@ -1,7 +1,13 @@
 package com.devhjs.randompick
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class RandomPickApp : Application()
+class RandomPickApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MobileAds.initialize(this)
+    }
+}
