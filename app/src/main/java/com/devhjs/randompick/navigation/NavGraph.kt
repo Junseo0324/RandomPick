33abaa7ba6397009630cd9421774168a99270378
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-19 18:33:05 +0900

    feat_v2: 탭 전환 시 디졸브/페이드 처리

--- app/src/main/java/com/devhjs/randompick/navigation/NavGraph.kt
+++ app/src/main/java/com/devhjs/randompick/navigation/NavGraph.kt
@@ -1,5 +1,7 @@
 package com.devhjs.randompick.navigation
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -10,7 +12,14 @@ import com.devhjs.randompick.navigation.data.Screen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Main.route) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Main.route,
+        enterTransition = { EnterTransition.None},
+        exitTransition = { ExitTransition.None},
+        popEnterTransition = { EnterTransition.None},
+        popExitTransition = { ExitTransition.None}
+        ) {
         composable(Screen.Main.route) { MainScreen(navController) }
         composable(Screen.List.route) { ListScreen() }
     }
