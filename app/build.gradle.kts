5a6afb9f4d301b1e9fa40f0c6ef3df68ff15999e
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-12-15 16:01:26 +0900

    fix/gradle : 오류 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.devhjs.randompick"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["adMobAppId"] = adMobAppId
